4c751d79c09919cea23654ebe259a94466bee5c4
Javier Aguilar <javier@anid.es>
2012-11-01 23:58:31 +0100

    no message

--- bin/install.sh
+++ bin/install.sh
@@ -17,7 +17,7 @@ if [ ! -d "src/public" ]; then
     git clone https://github.com/kometphp/assets.git
     mv assets public
     chmod 0755 public
-    rm -rf public/.git public/index.php public/.gitignore public/.gitattributes
+    rm -rf public/.git public/index.html public/.gitignore public/.gitattributes
     cp system/vendor/kometphp/core/index.default.php public/index.php
 fi
 
